789d2a0b57eca558130e43ce485fb12054ecc7f5
LELLO2 <114951822+LELLO2@users.noreply.github.com>
2022-10-12 22:26:11 +0200

    Add files via upload

---
+++ strem.vb/strem.vb/Form1.vb
@@ -0,0 +1,28 @@
+﻿Public Class Form1
+    Public R As New Random
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Me.Timer1.Start()
+
+    End Sub
+
+
+    Public i As Integer
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+
+        i = i + 1
+
+        Dim Student = "Height" & i
+
+        Dim ValueOfHigh As Integer = Me.R.Next(155, 200)
+
+        Me.RichTextBox1.AppendText(Student.PadRight(8) & Environment.NewLine & ValueOfHigh & Environment.NewLine)
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+        Me.Timer1.Stop()
+
+    End Sub
+End Class
